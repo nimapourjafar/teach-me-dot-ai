ab5ff73395bfbf6c7938f0654e4cf91a3f0b66e0
Nima Pourjafar <nima.pourjafar123@gmail.com>
2023-03-03 21:14:30 -0500

    added requirements.txt

--- server/qa.py
+++ server/qa.py
@@ -15,8 +15,8 @@ with open("server/faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
     store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+chain = VectorDBQAWithSourcesChain.from_chain_type(llm=OpenAI(temperature=0), chain_type="map_reduce",vectorstore=store)
 
-result = chain({"question": "What are Ray Dalio's principles?"})
+result = chain({"question": "What are principles? (Include a citation from the text)"})
 
 print(f"Answer: {result['answer']}")
